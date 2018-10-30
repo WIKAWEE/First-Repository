be4221e0bb0f216ddc6f6fc0890890dc779be8c2
WIKAWEE <44576631+WIKAWEE@users.noreply.github.com>
2018-10-30 09:47:30 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello, World!");
     System.out.println("This should only print in the old version!");
-    System.out.println("Oops I accidentally finished the pull request, will get it back up soon");
+    System.out.println("Oops I accidentally merged the pull request and it went away, but now another one should be up.");
   }
 }
