1866e56b9a991b82a1e1b0b5f9145a9e7d555695
WIKAWEE <44576631+WIKAWEE@users.noreply.github.com>
2018-10-30 09:43:08 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello, World!");
-    System.out.println("This should only print in the new, edited version!");
+    System.out.println("This should only print in the old version!");
   }
 }
